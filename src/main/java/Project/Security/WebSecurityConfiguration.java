a8b53ee89260d5333ea7ea4e03b9f5d264030725
binq1000 <t.daniels@student.fontys.nl>
2017-06-10 03:37:44 +0200

    Added login in for Police application.

---
+++ src/main/java/Project/Security/WebSecurityConfiguration.java
@@ -0,0 +1,48 @@
+package Project.Security;
+
+import Project.DAO.AccountDao;
+import com.S63B.domain.Entities.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * Created by Nekkyou on 6-6-2017.
+ */
+@Configuration
+public class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+	@Autowired
+	private AccountDao accountDao;
+
+	@Override
+	public void init(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService());
+	}
+
+	@Bean
+	UserDetailsService userDetailsService() {
+		return new UserDetailsService() {
+
+			@Override
+			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+				Account account = accountDao.findByUsername(username);
+				if(account != null) {
+					return new User(account.getUsername(), account.getPassword(), true, true, true, true,
+							AuthorityUtils.createAuthorityList("USER"));
+				} else {
+					throw new UsernameNotFoundException("could not find the user '"
+							+ username + "'");
+				}
+			}
+
+		};
+	}
+}
