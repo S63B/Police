245c29adc33a2f8dc12744cedc9219ec0ec2d2f9
arjan <arjan_vandermeer@live.nl>
2017-06-13 22:11:52 +0200

    .

--- src/main/java/Project/Security/SpringSecurityConfig.java
+++ src/main/java/Project/Security/SpringSecurityConfig.java
@@ -1,29 +1,29 @@
-package Project.Security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-/**
- * Created by Nekkyou on 6-6-2017.
- */
-//@Configuration
-//@EnableWebSecurity
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.antMatchers(HttpMethod.OPTIONS, "/*").permitAll()
-				.anyRequest()
-				.fullyAuthenticated()
-				.and()
-			.httpBasic()
-				.and()
-			.csrf()
-			.disable();
-	}
-}
+//package Project.Security;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+///**
+// * Created by Nekkyou on 6-6-2017.
+// */
+////@Configuration
+////@EnableWebSecurity
+//public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+////	@Override
+////	protected void configure(HttpSecurity http) throws Exception {
+////		http
+////			.authorizeRequests()
+////				.antMatchers(HttpMethod.OPTIONS, "/*").permitAll()
+////				.anyRequest()
+////				.fullyAuthenticated()
+////				.and()
+////			.httpBasic()
+////				.and()
+////			.csrf()
+////			.disable();
+////	}
+//}
