d2f14f11a18ea0f29a18e79d9ad453f98d5b870b
Bram de Boer <bramdeboer@outlook.com>
2017-06-20 16:57:41 +0200

    enviroment fix

--- PoliceAngular/src/environments/environment.prod.ts
+++ PoliceAngular/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   policeUrl:"http://192.168.24.120:8090/police",
   trackingUrl:"http://192.168.24.120:8080",
+  adminUrl:"http://192.168.24.120:8082",
   envName: 'prod'
 };
